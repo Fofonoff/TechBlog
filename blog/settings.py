0e01a243b015a48391cfc52290fda4af228b6cd2
Fofonoff <joni.fofonoff@myy.haaga-helia.fi>
2018-12-06 16:49:29 +0200

    removed sk

--- blog/settings.py
+++ blog/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '3y($380j6#*kh=9(amlz!10__#!sfl8=8l3l-96^a3kg0*^7z6'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
