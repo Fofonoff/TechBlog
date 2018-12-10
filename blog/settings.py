b2a7b61cbc96414cfff8b208bcf2dc87098c0a22
Fofonoff <joni.fofonoff@myy.haaga-helia.fi>
2018-12-10 03:41:57 +0200

    changed settings to enable deployment

--- blog/settings.py
+++ blog/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '3y($380j6#*kh=9(amlz!10__#!sfl8=8l3l-96^a3kg0*^7z6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['68.183.218.24']
 
